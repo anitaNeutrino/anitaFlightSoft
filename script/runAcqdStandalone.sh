de726398f676e6af1510aef41c8e4c8f5f1d1a98
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2005-08-15 21:57:00 +0000

    Adding script

---
+++ script/runAcqdStandalone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+rm -rf /tmp/anita
+echo 0 > /home/rjn/flightSoft/lastEventNumber
+killAllProgs.sh
+xterm -T firstAcqd -e "firstAcqd ; read" &
+#xterm -T GPSd -e "GPSd ; read" &
+#xterm -T Eventd -e "Eventd ; read" &
+#xterm -T Prioritizerd -e "Prioritizerd ; read" &
+#xterm -T Archived -e "Archived ; read" &
