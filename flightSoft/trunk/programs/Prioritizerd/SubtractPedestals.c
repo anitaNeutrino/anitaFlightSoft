e8c6731be04de740be04c203940fdd8f5988f83b
beatty <beatty@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-04-21 14:23:38 +0000

    Oops...need to add the code

---
+++ flightSoft/trunk/programs/Prioritizerd/SubtractPedestals.c
@@ -0,0 +1,69 @@
+#include "SubtractPedestals.h"
+
+Fixed8_t scaledPed[ACTIVE_SURFS][LABRADORS_PER_SURF]
+[CHANNELS_PER_SURF][MAX_NUMBER_SAMPLES]; 
+/* precomputed integer fixed point pedestals */
+
+int readPedestals(int pedOption)
+{
+     int surf,lab,chan,samp;
+     switch (pedoption){
+     case 0:
+	  break;
+     case 1:
+	  break;
+     case 2:
+	  break;
+     case 3:
+	  break;
+     default:
+	  do (surf=0; surf<ACTIVE_SURFS; surf++){
+	       do (lab=0;lab<LABRADORS_PER_SURF;lab++){
+		    do (chan=0; chan<CHANNELS_PER_SURF;chan++){
+			 do (samp=0;samp<MAX_NUMBER_SAMPLES;samp++){
+			      currentPedestal.unixTime=0;
+			      currentPedestal.nsamples=0;
+			      currentPedestal.thePeds[surf][lab][chan][samp]
+				   =0.0;
+			      currentPedestal.pedsRMS[surf][lab][chan][samp]
+				   =0.0;
+			 }
+		    }
+	       }
+	  }
+     }  
+     /* precompute the scaled pedestals */
+     do (surf=0; surf<ACTIVE_SURFS; surf++){
+	  do (lab=0;lab<LABRADORS_PER_SURF;lab++){
+	       do (chan=0; chan<CHANNELS_PER_SURF;chan++){
+		    do (samp=0;samp<MAX_NUMBER_SAMPLES;samp++){
+			 scaledPed[surf][lab][chan][samp]=
+			      (int)(256*currentPedestal.thePeds
+				    [surf][lab][chan][samp]);
+		    }
+	       }
+	  }
+     }
+}
+
+int subtractPedestals(AnitaEventBody_t *rawSurfEvent,
+		      AnitaTransientBody8_t *SurfTransientPS)
+{
+     int digCh, surf,chan,chip,samp;
+     unsigned char chanId,chipIdFlag;
+     do (digCh=0;digCh<NUM_DIGITIZED_CHANNELS;digch++){
+	  chanId=((rawSurfEvent->channel[digCh]).header).chanId;
+	  chipIdFlag=((rawSurfEvent->channel[digCh]).header).chipIdFlag;
+	  /* don't think I need the hitbus stuff here */
+	  surf=chanId/9; chan=chanid%9;
+	  chip=(chipIdFlag & 0x3);
+	  /* Pedestals are scaled to Fixed_8 already */
+	  do (samp=0; samp<MAX_NUMBER_SAMPLES; samp++){
+	       (SurfTransientPS->ch[digCh]).data[samp]=
+		    256*(rawSurfEvent->ch[digCh]).data[samp]
+		    - scaledPed[surf][chip][chan][samp];
+	  }
+	  (SurfTransientPS->ch[digCh]).valid_samples=-1;
+	  /* -1 indicates this has not been unwrapped yet */
+     }
+}
