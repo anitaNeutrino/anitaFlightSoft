32ef4978471f8ce29cb9b8429e33e08ca54ace3e
beatty <beatty@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-05-18 16:58:41 +0000

    Median finding code and test routine.

---
+++ programs/Compressd/testmedian.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "findmedian.h"
+
+int main(int argc, char* argv[]){
+     unsigned short nwords;
+     unsigned short testword[65536];
+     unsigned short median;
+     short i;
+     if (argc!=2){
+	  fprintf(stderr,"Usage: testmedian <1/2 number of words>");
+	  exit(-1);
+     }
+     sscanf(argv[1],"%hu",&nwords);
+     for (i=0;i<nwords;i++){
+	  testword[i]=i;
+	  testword[2*nwords-(i+1)]=i;
+     }
+     median=findmedian(2*nwords,testword);
+     printf("%hu\n",median);
+}
