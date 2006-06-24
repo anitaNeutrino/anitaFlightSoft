f8ca4e853f3842ffc77916fc500622cd17717be3
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-06-24 02:00:52 +0000

    Adding debugging to Hkd

--- flightSoft/trunk/standard_definitions.mk
+++ flightSoft/trunk/standard_definitions.mk
@@ -9,7 +9,7 @@ ANITA_BIN_DIR=${ANITA_FLIGHT_SOFT_DIR}/bin
 ObjSuf	      = o
 SrcSuf        = c
 ExeSuf        =
-OPT           = -O2 -Wall #-DGCC #-g --debug --pedantic-errors
+OPT           = -O2 -Wall -g -pg# --debug --pedantic-errors
 
 NOOPT         =
 EXCEPTION     = 
@@ -17,9 +17,9 @@ EXCEPTION     =
 CC           = gcc
 INCLUDES      = -I$(ANITA_FLIGHT_SOFT_DIR) -I$(ANITA_FLIGHT_SOFT_DIR)/common \
 -I$(ANITA_FLIGHT_SOFT_DIR)/common/includes -I$(ANITA_FLIGHT_SOFT_DIR)/outside/cr7/include -I$(ANITA_FLIGHT_SOFT_DIR)/outside/acromag -I$(ANITA_FLIGHT_SOFT_DIR)/common/sipcomLib
-CCFLAGS      = $(EXCEPTION) $(OPT) -fPIC $(INCLUDES) -D_BSD_SOURCE
+CCFLAGS      = $(EXCEPTION) $(OPT) -fPIC $(INCLUDES) -D_BSD_SOURCE 
 LD            = gcc
-LDFLAGS       = $(EXCEPTION) -L$(ANITA_LIB_DIR)
+LDFLAGS       = $(EXCEPTION) -L$(ANITA_LIB_DIR) 
 ANITA_LIBS    =  -lkvp -lConfig -lUtil -lcr7 -lm -lz #-lSocket
 ANITA_HKD_LIBS = -lAcromag
 ANITA_GPS_LIBS = -lSerial
