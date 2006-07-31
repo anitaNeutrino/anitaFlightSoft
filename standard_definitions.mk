e91a026232fbfde38877bc95a460d8b48698b690
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-07-31 15:13:42 +0000

    Removing line from standard_definitions.mk that was causing errors

--- standard_definitions.mk
+++ standard_definitions.mk
@@ -9,7 +9,7 @@ ANITA_BIN_DIR=${ANITA_FLIGHT_SOFT_DIR}/bin
 ObjSuf	      = o
 SrcSuf        = c
 ExeSuf        =
-OPT           = -O2 -Wall -g -std=c99 # --debug --pedantic-errors
+OPT           = -O2 -Wall -g # --debug --pedantic-errors
 
 NOOPT         =
 EXCEPTION     = 
