00d4311ca54cd0a959530f4b8df7352c217b8265
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-08-04 22:03:17 +0000

    ng up messZZ

--- script/anitaStartScript.sh
+++ script/anitaStartScript.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-daemon -u anita -r Archived -n Archived
-daemon -u anita -r Acqd -n Acqd
-daemon -u anita -r Eventd -n Eventd
-daemon -u anita -r GPSd -n GPSd
-daemon -u anita -r Hkd -n Hkd
-daemon -u anita -r LOSd -n LOSd
-daemon -u anita -r Prioritizerd -n Prioritizerd
-daemon -u anita -r Monitord -n Monitord
-daemon -u anita -r Calibd -n Calibd
+#daemon -u anita -r Archived -n Archived
+#daemon -u anita -r Acqd -n Acqd
+#daemon -u anita -r Eventd -n Eventd
+#daemon -u anita -r GPSd -n GPSd
+#daemon -u anita -r Hkd -n Hkd
+#daemon -u anita -r LOSd -n LOSd
+#daemon -u anita -r Prioritizerd -n Prioritizerd
+#daemon -u anita -r Monitord -n Monitord
+#daemon -u anita -r Calibd -n Calibd
 daemon -u anita -r SIPd -n SIPd
 daemon -u anita -r Cmdd -n Cmdd
