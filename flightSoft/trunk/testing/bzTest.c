7cfbdfb73f1333c7156b84d1e9e2d3d7fda54d00
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-08-15 14:04:08 +0000

    Added a labPed dump facility in testing.

--- flightSoft/trunk/testing/bzTest.c
+++ flightSoft/trunk/testing/bzTest.c
@@ -24,10 +24,12 @@ int main(int argc, char**argv) {
 	printf("Usage: %s <event telem file>\n",basename(argv[0]));
 	return 0;
     }
+
+    EncodedPedSubbedSurfPacketHeader_t *surfHdPtr;
     char buffer[100000];
     char output[200000];
     unsigned int inLength=100000,outLength=200000;
-    int count=0;
+    int count=0,surf;
     int retVal=0;
     char filename[FILENAME_MAX];
     sprintf(filename,"%s",argv[1]);
@@ -36,10 +38,13 @@ int main(int argc, char**argv) {
     printf("Read %d bytes\n",numBytes);
     if(numBytes<0) return 0;
 
-
-    retVal=BZ2_bzBuffToBuffCompress(output,&outLength,buffer,inLength,1,0,30);
-
-    printf("In length %u, Out Length %u\n",inLength,outLength);
+    for(surf=0;surf<ACTIVE_SURFS;surf++) {
+	surfHdPtr = (EncodedPedSubbedSurfPacketHeader_t*) &buffer[count];
+	inLength=surfHdPtr->gHdr.numBytes;
+	retVal=BZ2_bzBuffToBuffCompress(output,&outLength,buffer,inLength,1,0,100);
+	printf("In length %u, Out Length %u\n",inLength,outLength);
+	count+=inLength;
+    }
 
     return 1;
 }
