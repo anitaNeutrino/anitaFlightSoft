2ce608dc00c802e2cd2620b021aafb1f4ebb9c5f
beatty <beatty@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-08-16 20:13:04 +0000

    added fftw cross correlator

--- programs/Prioritizerd/Filters.c
+++ programs/Prioritizerd/Filters.c
@@ -1,5 +1,8 @@
 #include <anitaFlight.h>
 #include "Filters.h"
+#include "AnitaInstrument.h"
+#include "fftwxcor.h"
+#include <stdio.h>
 
 void NullFilterAll(AnitaTransientBody8_t *in, AnitaTransientBody3_t *out)
 {
@@ -20,4 +23,48 @@ void NullFilter(TransientChannel8_t *inch,TransientChannel3_t *outch)
      outch->valid_samples=inch->valid_samples;
 }
 
-// the matched filter will go here--need to make a template.
+
+void HornMatchedFilter(TransientChannelF_t *inch,TransientChannelF_t *outch)
+{
+     int status,i;
+     static int first=1;
+     static float template[256];
+     float corr_out[512];
+     FILE *fptmpl;
+     if (first){
+	  //read in template
+	  fptmpl=fopen("horn_template.dat","r");
+	  if (fptmpl==NULL){
+	       fprintf(stderr,"Unable to open horn template.\n");
+	       exit(-1);
+	  }
+	  for (i=0;i<256;i++){
+	       fscanf(fptmpl,"%f",&(template[i]));
+	  }
+	  fclose(fptmpl);
+	  first=0;
+     }
+     fftwcorr(inch->data,template,corr_out);
+     for (i=0;i<inch->valid_samples;i++){
+	  outch->data[i]=corr_out[i];
+     }
+     outch->valid_samples=inch->valid_samples;
+}
+
+void HornMatchedFilterAll(AnitaInstrumentF_t *in, 
+			  AnitaInstrumentF_t *out)
+{
+     int i,j;
+     for (i=0;i<16;i++){
+	  for (j=0;j<2;j++){
+	       HornMatchedFilter(&(in->topRing[i][j]),&(out->topRing[i][j]));
+	       HornMatchedFilter(&(in->botRing[i][j]),&(out->botRing[i][j]));
+	  }
+     }
+     for (i=0;i<4;i++){
+	  HornMatchedFilter(&(in->bicone[i]),&(out->bicone[i]));
+	  HornMatchedFilter(&(in->discone[i]),&(out->discone[i]));
+     }
+			    
+}
+     
---
+++ programs/Prioritizerd/fftwxcor.h
@@ -0,0 +1,13 @@
+#ifndef FFTWXCOR_H
+#define FFTWXCOR_H
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int fftwcorr(float wfm[], float tmpl[], float cout[]);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
