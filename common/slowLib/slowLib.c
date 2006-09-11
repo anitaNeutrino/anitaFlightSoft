6dad3d3bf2e14e546863fe5b3be51b9c2bc0334f
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-09-11 17:37:43 +0000

    Fixed bug in setting antTrigMask with Cmdd, and tidied up a little

--- common/slowLib/slowLib.c
+++ common/slowLib/slowLib.c
@@ -11,3 +11,36 @@
 #include "slowLib/slowLib.h"
 
 
+typedef struct {
+    float rfPwr[ACTIVE_SURFS][RFCHAN_PER_SURF];
+    float scalerRates[TRIGGER_SURFS][ANTS_PER_SURF]; 
+    float scalerRatesSq[TRIGGER_SURFS][ANTS_PER_SURF]; 
+    float upperL2Rates[PHI_SECTORS];
+    float lowerL2Rates[PHI_SECTORS];
+    float l3Rates[PHI_SECTORS];    
+} SlowRateRFCalcStruct_t
+
+
+FullSurfHkStruct_t theSurfHks[300];
+TurfRateStruct_t theTurfRates[300];
+int numSurfHks=0;
+int numTurfRates=0;
+int currentSurfHk=0;
+int currentTurfRate;
+
+
+
+void addSurfHkToAverage(FullSurfHkStruct_t *hkStruct)
+{
+
+
+}
+
+
+void addTurfRateToAverage(TurfRateStruct_t *turfRate)
+{
+
+
+}
+
+
