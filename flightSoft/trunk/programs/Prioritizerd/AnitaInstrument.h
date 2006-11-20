e851e05442431607c551f67c711821a3157bff16
beatty <beatty@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-11-20 08:24:31 +0000

    Lots of bugfixes to first cut at baseline fitting.

--- flightSoft/trunk/programs/Prioritizerd/AnitaInstrument.h
+++ flightSoft/trunk/programs/Prioritizerd/AnitaInstrument.h
@@ -58,6 +58,37 @@ typedef struct{
      Peak_t discone[4];
 } AnitaPeak_t;
 
+#define MAX_BASELINES 16
+#define MAX_ITER 10
+
+typedef struct{
+     int nbaselines; //filled by builder
+     int ngood; //count the baselines that are not bad.  
+                //If there are too few to perform the fit, 
+                //don't fit.  Filled by fitter.
+     int validfit; //zero for good fit; otherwise an error code
+     //-1 : a singular matrix was found in the initial fit
+     //-2 : a singular matrix was found in the Lagrange multiplier search
+     //-3 : too many iterations in the Lagrange search
+     //-4 : too few baselines to fit
+     int niter; //number of itereations to get norm right
+     float lambda; //Lagrange multiplier for norm constraint
+     float arrival[3]; //best fit to vector from instrument to
+                       //source, normalized on the unit sphere
+     float norm; //fitted length of direction vector
+     float position[MAX_BASELINES][2][3]; //convenience copies of
+     float delay[MAX_BASELINES][2]; //time in meters
+     float direction[MAX_BASELINES][3]; //unit vector from 
+                                        //late to early antenna
+     float length[MAX_BASELINES]; //length of baseline
+     float sinangle[MAX_BASELINES]; //sine has errors which 
+                                    //are linear in delay
+     float cosangle[MAX_BASELINES]; //used for statB
+     int bad[MAX_BASELINES]; //nonzero for 'bad' baselines
+                             //integer gives pass on which baseline
+                             //was rejected
+}BaselineAnalysis_t;
+
 extern const int topRingIndex[PHI_SECTORS][2];
 extern const int botRingIndex[PHI_SECTORS][2];
 extern const int biconeIndex[4]; //phi=2,6,10,14
@@ -111,7 +142,11 @@ extern "C"
      void FormSectorMajority(AnitaChannelDiscriminator_t *in,
 			     AnitaSectorLogic_t *out,
 			     int sectorWidth);
-			    
+
+     void FindPeaks(AnitaInstrumentF_t *theInst, AnitaPeak_t *thePeak);
+
+     void MakeBaselinesFour(AnitaPeak_t *thePeak,BaselineAnalysis_t *theBA,
+			    int phi,int dphi);			    
 #ifdef __cplusplus
 }
 #endif
--- flightSoft/trunk/programs/Prioritizerd/BaselineAnalysis.h
+++ flightSoft/trunk/programs/Prioritizerd/BaselineAnalysis.h
@@ -4,37 +4,6 @@
 #include <anitaFlight.h>
 #include "AnitaInstrument.h"
 
-#define MAX_BASELINES 16
-#define MAX_ITER 10
-
-typedef struct {
-     int nbaselines; //filled by builder
-     int ngood; //count the baselines that are not bad.  
-                //If there are too few to perform the fit, 
-                //don't fit.  Filled by fitter.
-     int validfit; //zero for good fit; otherwise an error code
-     //-1 : a singular matrix was found in the initial fit
-     //-2 : a singular matrix was found in the Lagrange multiplier search
-     //-3 : too many iterations in the Lagrange search
-     //-4 : too few baselines to fit
-     int niter; //number of itereations to get norm right
-     float lambda; //Lagrange multiplier for norm constraint
-     float arrival[3]; //best fit to vector from instrument to
-                       //source, normalized on the unit sphere
-     float norm; //fitted length of direction vector
-     float position[MAX_BASELINES][2][3]; //convenience copies of
-     float delay[MAX_BASELINES][2]; //time in meters
-     float direction[MAX_BASELINES][3]; //unit vector from 
-                                        //late to early antenna
-     float length[MAX_BASELINES]; //length of baseline
-     float sinangle[MAX_BASELINES]; //sine has errors which 
-                                    //are linear in delay
-     float cosangle[MAX_BASELINES]; //used for statB
-     int bad[MAX_BASELINES]; //nonzero for 'bad' baselines
-                             //integer gives pass on which baseline
-                             //was rejected
-} BaselineAnalysis_t;
-    
 #ifdef __cplusplus
 extern "C"
 {
@@ -42,7 +11,7 @@ extern "C"
      float det3(float a[3][3]);
      float det3p(float *a[3]);
      int solve3(float a[3][3], float b[3], float x[3]);
-     int fitBaselines(BaselineAnalysis_t *theBL);
+     int FitBaselines(BaselineAnalysis_t *theBL);
 #ifdef __cplusplus
 }
 #endif
