27413bd0767c912d4e81ce9fbdc90b7f8ee3cd36
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-12-05 22:04:08 +0000

    Possible flight condition --try2

--- flightSoft/trunk/script/anitaStartScript.sh
+++ flightSoft/trunk/script/anitaStartScript.sh
@@ -10,6 +10,17 @@ export LD_LIBRARY_PATH=${ANITA_FLIGHT_SOFT_DIR}/lib:${LD_LIBRARY_PATH}
 turnAllOff
 turnGPSOn
 
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Acqd.config.0 /home/anita/flightSoft/config/Acqd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Archived.config.0 /home/anita/flightSoft/config/Archived.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Calibd.config.0 /home/anita/flightSoft/config/Calibd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Eventd.config.0 /home/anita/flightSoft/config/Eventd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/GPSd.config.0 /home/anita/flightSoft/config/GPSd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/LOSd.config.0 /home/anita/flightSoft/config/LOSd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Monitord.config.0 /home/anita/flightSoft/config/Monitord.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Playbackd.config.0 /home/anita/flightSoft/config/Playbackd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/Priroitizerd.config.0 /home/anita/flightSoft/config/Prioritizerd.config
+sudo -u anita cp /home/anita/flightSoft/config/defaults/SIPd.config.0 /home/anita/flightSoft/config/SIPd.config
+
 #Start Cmdd and SIPd
 nice -n -20 daemon -u anita -r SIPd -n SIPd
 nice -n -20 daemon -u anita -r Cmdd -n Cmdd
@@ -36,7 +47,8 @@ daemon -u anita -r Eventd -n Eventd
 daemon -u anita -r GPSd -n GPSd
 daemon -u anita -r Hkd -n Hkd
 daemon -u anita -r LOSd -n LOSd
-daemon -u anita -r Prioritizerd -n Prioritizerd
+nice -n -5 daemon -u anita -r Prioritizerd -n Prioritizerd
 daemon -u anita -r Monitord -n Monitord
 daemon -u anita -r Calibd -n Calibd
+daemon -u anita -r Playbackd -n Playbackd
 
