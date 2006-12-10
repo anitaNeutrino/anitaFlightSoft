a0b8276602ec7e8a7c6344af04c7d4546893f4d4
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2006-12-10 17:04:38 +0000

    Fixed closing bug in configStore

--- flightSoft/trunk/testing/sillyTest.c
+++ flightSoft/trunk/testing/sillyTest.c
@@ -76,6 +76,7 @@ int main (void)
     printf("Size of QueueStruct_t: %d\n",sizeof(QueueStruct_t));
     printf("Size of MonitorStruct_t: %d\n",sizeof(MonitorStruct_t));
     printf("Size of OtherMonitorStruct_t: %d\n",sizeof(OtherMonitorStruct_t));
+    printf("Size of RunStart_t: %d\n",sizeof(RunStart_t));
     printf("Size of LabChipChannelPedStruct_t: %d\n",sizeof(LabChipChannelPedStruct_t));
     printf("Size of FullLabChipPedStruct_t: %d\n",sizeof(FullLabChipPedStruct_t));
     printf("Size of FullPedStruct_t: %d\n",sizeof(FullPedStruct_t));
