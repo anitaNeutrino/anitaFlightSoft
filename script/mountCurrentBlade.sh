262d42e459bf3f61c0bc36d114e067e7d2a2f615
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2008-05-04 17:24:22 +0000

    Tweaking the disk mount scripts

--- script/mountCurrentBlade.sh
+++ script/mountCurrentBlade.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sudo umount /mnt/blade
+if df -h | grep -q "blade"
+then
+    sudo umount /mnt/blade
+fi
 
 disabled=`getConfigValue anitaSoft.config global disableBlade`
 if [ $disabled -eq 1 ] ; then
@@ -12,7 +15,7 @@ fi
 sudo chmod a-w /mnt/blade
 label=`getConfigString anitaSoft.config global bladeName`
 echo "Trying to mount $label on /mnt/blade"
-sudo mount -L $label -o defaults,sync /mnt/blade
+sudo mount -L $label -o defaults /mnt/blade
 if df -h | grep -q "blade"
 then
 	sudo chmod a+wrx /mnt/blade
