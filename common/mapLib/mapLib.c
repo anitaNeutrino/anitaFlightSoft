db6e1f4f6f3d1f6e4cdd72c720ca1f3591d7f231
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2008-07-01 13:39:16 +0000

    Adding Chris Williams code to determine phi position and azimuth of sources during flight

--- common/mapLib/mapLib.c
+++ common/mapLib/mapLib.c
@@ -14,8 +14,8 @@
 
 //Constants
 //const double M_PI=3.14159265358979;
-const double RADTODEG=M_PI/180.;   // radians/degree  
-const double DEGTORAD=180./M_PI;    // degree/rad
+const double DEGTORAD=M_PI/180.;   // radians/degree  
+const double RADTODEG=180./M_PI;    // degree/rad
 
 void fillPhiFromSurf(SurfAntMapStruct_t *mapPtr) {
     mapPtr->phi=-1;
@@ -125,8 +125,8 @@ int getLogicalIndexFromPhi(int phi,  AntennaTier_t tier, AntennaPol_t pol)
 void ltLgAlToGeoXYZ(double latLonAlt[3], double xyz[3])
 {
   double lat, lon, h;
-  lat = latLonAlt[0]*RADTODEG;
-  lon = latLonAlt[1]*RADTODEG;
+  lat = latLonAlt[0]*DEGTORAD;
+  lon = latLonAlt[1]*DEGTORAD;
   h   = latLonAlt[2];
   double r = 6378137.0;//Earth's equatorial radius
   double f = 1/298.257223563;//flattening factor
@@ -170,8 +170,8 @@ void geoXYZToLtLgAl(double xyz[3], double latLonAlt[3])
   double h = (sqrt(x*x + y*y)/cos(lat)) - nu;
   
     
-  latLonAlt[0] = lat*DEGTORAD;
-  latLonAlt[1] = lon*DEGTORAD;
+  latLonAlt[0] = lat*RADTODEG;
+  latLonAlt[1] = lon*RADTODEG;
   latLonAlt[2] = h;
 }
 
@@ -183,8 +183,8 @@ void geoXYZToAXYZ(double xyz[3], double aLtLgAl[3], double hpr[3], double axyz[3
   double aNWU[3]={0};
   double apXYZ[3]={0};
 
-  double lat = abs(aLtLgAl[0])*RADTODEG;
-  double lon = abs(aLtLgAl[1])*RADTODEG;
+  double lat = abs(aLtLgAl[0])*DEGTORAD;
+  double lon = abs(aLtLgAl[1])*DEGTORAD;
   ltLgAlToGeoXYZ(aLtLgAl,apXYZ);
 
   double x = (xyz[0] - apXYZ[0]);
@@ -197,26 +197,114 @@ void geoXYZToAXYZ(double xyz[3], double aLtLgAl[3], double hpr[3], double axyz[3
 
   //Need to add something that converts from aNWU to aXYZ and fix all the 
   // silly naming things I can't remember
-  //  out = aNWUToAXYZ(aNWU, hpr);
-  //  return out;
+  aNWUToAXYZ(aNWU,hpr,axyz);
 }
 
 void aXYZToGeoXYZ(double axyz[3], double aLtLgAl[3], double hpr[3], double xyz[3])
 {
+  double aNWU[3]={0};
+  double apXYZ[3]={0};
+  xyz[0]=0;
+  xyz[1]=0;
+  xyz[2]=0;
 
+  double lat = abs(aLtLgAl[0])*DEGTORAD;
+  double lon = abs(aLtLgAl[1])*DEGTORAD;
+  aXYZToANWU(xyz, hpr, aNWU);
+  ltLgAlToGeoXYZ(aLtLgAl,apXYZ);
+  
+  xyz[0]= cos(lon)*aNWU[0] - sin(lon)*sin(lat)*aNWU[1] + sin(lon)*cos(lat)*aNWU[2] + apXYZ[0];
+  xyz[1]= sin(lon)*aNWU[0] + cos(lon)*sin(lat)*aNWU[1] - cos(lon)*sin(lat)*aNWU[2] + apXYZ[1];
+  xyz[2]= cos(lat)*aNWU[1] - sin(lat)*aNWU[2] + apXYZ[2];
 
 
 }
 
-int getSourcePhiAndDistance(double anitaLatLonAltHead[4],
+void aXYZToANWU(double aXYZ[3], double hpr[3], double aNWU[3])
+{
+  double a = hpr[0]*DEGTORAD -M_PI/4;
+  double b = hpr[1]*DEGTORAD;
+  double c = hpr[2]*DEGTORAD;
+
+  /* conversion for aXYZ to aNWU using rotation matrices as follows:
+   * 
+   *            aNWU = RhRpRr.aXYZ
+   *
+   *		Rh >= heading rotation
+   * 		Rp >= pitch rotation
+   *		Rr >= roll rotation
+   *
+   */
+
+
+  aNWU[0]=  cos(a)*cos(b)*aXYZ[0] + (sin(a)*cos(c) - cos(a)*sin(b)*sin(c))*aXYZ[1] + (-sin(a)*sin(c)-cos(a)*sin(b)*cos(c))*aXYZ[2];
+  aNWU[1]=  -sin(a)*cos(b)*aXYZ[0] + (cos(a)*cos(c) + sin(a)*sin(b)*sin(c))*aXYZ[1] + (-cos(a)*sin(c) + sin(a)*sin(b)*cos(c))*aXYZ[2];
+  aNWU[2]=  sin(b)*aXYZ[0] + sin(c)*cos(b)*aXYZ[1] + cos(b)*cos(c)*aXYZ[2];
+
+
+}
+
+void aNWUToAXYZ(double aNWU[3], double hpr[3], double aXYZ[3])
+{
+  double a = hpr[0]*DEGTORAD - M_PI/4;
+  double b = hpr[1]*DEGTORAD;
+  double c = hpr[2]*DEGTORAD;
+
+  /* conversion for aNWU to aXYZ using inverse rotation matrices denoted as inv(Rx)
+   *
+   *            aXYZ = inv(Rr)inv(Rp)inv(Rh).aNWU
+   *
+   */
+
+  aXYZ[0]= cos(a)*cos(b)*aNWU[0] - cos(b)*sin(a)*aNWU[1] - sin(b)*aNWU[2];
+  aXYZ[1]= (sin(c)*sin(b)*cos(a) + cos(c)*sin(a))*aNWU[0] + (-sin(c)*sin(b)*sin(a) + cos(c)*cos(a))*aNWU[1] + cos(b)*sin(c)*aNWU[2];
+  aXYZ[2]= (cos(c)*sin(b)*cos(a) - sin(c)*sin(a))*aNWU[0] + (-cos(c)*sin(b)*sin(a) - sin(c)*cos(a))*aNWU[1] + cos(c)*cos(b)*aNWU[2];
+
+
+}
+
+
+int getSourcePhiAndDistance(double anitaLatLonAlt[3],
+			    double anitaHPR[3],
 			    double sourceLatLonAlt[3],
 			    int *phiDir,
 			    double *distance)
 {
   //
+  double anitaXYZ[3]={0};
+  double sourceXYZ[3]={0};
+  double sourceAXYZ[3]={0};
+  ltLgAlToGeoXYZ(anitaLatLonAlt,anitaXYZ);
+  ltLgAlToGeoXYZ(sourceLatLonAlt,sourceXYZ);
+  geoXYZToAXYZ(sourceXYZ,anitaLatLonAlt,anitaHPR,sourceAXYZ);
+  
+  *distance=sqrt(sourceAXYZ[0]*sourceAXYZ[0]+
+		sourceAXYZ[1]*sourceAXYZ[1]+
+		sourceAXYZ[2]*sourceAXYZ[2]);
 
+  //Need to fix below to be robust and not seg v
+  double azimuth=RADTODEG * atan2(sourceAXYZ[1],sourceAXYZ[0]);
 
-  *phiDir=-1;
-  *distance=-1;
+  if(azimuth>11.25 && azimuth<=33.75)*phiDir=2;
+  else if(azimuth>33.75 && azimuth<=56.25)*phiDir=1;
+  else if(azimuth>56.25 && azimuth<=78.75)*phiDir=0;
+  else if(azimuth>78.75 && azimuth<=101.25)*phiDir=15;
+  else if(azimuth>101.25 && azimuth<=123.75)*phiDir=14;
+  else if(azimuth>123.75 && azimuth<=146.25)*phiDir=13;
+  else if(azimuth>146.25 && azimuth<=168.75)*phiDir=12;
+  else if(azimuth>168.75 && azimuth<=191.25)*phiDir=11;
+  else if(azimuth>191.25 && azimuth<=213.75)*phiDir=10;
+  else if(azimuth>213.75 && azimuth<=236.25)*phiDir=9;
+  else if(azimuth>236.25 && azimuth<=258.75)*phiDir=8;
+  else if(azimuth>258.75 && azimuth<=281.25)*phiDir=7;
+  else if(azimuth>281.25 && azimuth<=303.75)*phiDir=6;
+  else if(azimuth>303.75 && azimuth<=326.25)*phiDir=5;
+  else if(azimuth>326.25 && azimuth<=348.75)*phiDir=4;
+  else if(azimuth>348.75 && azimuth<=360)*phiDir=3;
+  else if(azimuth>=0 && azimuth<=11.25)*phiDir=3;
+  else printf("\n\nERROR OBJECT ANITA AZIMUTH OUT OF RANGE\n\n");
+
+  //  *phiDir=-1;
+  //  *distance=-1;
   return -1;
 }
