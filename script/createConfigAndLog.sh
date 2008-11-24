24ea2ab6e3a58e3910486f7d7d83be62098d7c21
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2008-11-24 20:32:59 +0000

    Changed simpleLog to simpleLog.txt

--- script/createConfigAndLog.sh
+++ script/createConfigAndLog.sh
@@ -1,37 +1,30 @@
 #!/bin/bash
 
 
-echo "User: Cmdd" > /tmp/simpleLog
-echo "Run Description:" >> /tmp/simpleLog
-echo "   Started by Cmdd" >> /tmp/simpleLog
+echo "User: Cmdd" > /tmp/simpleLog.txt
+echo "Run Description:" >> /tmp/simpleLog.txt
+echo "   Started by Cmdd" >> /tmp/simpleLog.txt
 
 
 if [ -d /mnt/data/current ]; then
     mkdir /mnt/data/current/log
-    cp /tmp/simpleLog /mnt/data/current/log/simpleLog.txt
+    cp /tmp/simpleLog.txt /mnt/data/current/log/simpleLog.txt
 fi
 
 if [ -d /mnt/satamini/current ]; then
     mkdir /mnt/satamini/current/log
-    cp /tmp/simpleLog /mnt/satamini/current/log
+    cp /tmp/simpleLog.txt /mnt/satamini/current/log
     mkdir /mnt/satamini/current/config
     cp /home/anita/flightSoft/config/*.config /mnt/satamini/current/config
 fi    
 
 if [ -d /mnt/satablade/current ]; then
     mkdir /mnt/satablade/current/log
-    cp /tmp/simpleLog /mnt/satablade/current/log
+    cp /tmp/simpleLog.txt /mnt/satablade/current/log
     mkdir /mnt/satablade/current/config
     cp /home/anita/flightSoft/config/*.config /mnt/satablade/current/config
 fi    
 
-
-if [ -d /mnt/usbint/current ]; then
-    mkdir /mnt/usbint/current/log
-    cp /tmp/simpleLog /mnt/usbint/current/log
-    mkdir /mnt/usbint/current/config
-    cp /home/anita/flightSoft/config/*.config /mnt/usbint/current/config
-fi    
 
 
 
