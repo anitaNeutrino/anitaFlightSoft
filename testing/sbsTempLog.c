d2f88b4e6ce7e82fe1a0695451880c60059d27bf
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2013-05-30 02:54:51 +0000

    Adding silly sbs temperature logger

---
+++ testing/sbsTempLog.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+#include <sys/types.h>
+
+#include <time.h>
+
+#include "sbsTempLib/sbsTempLib.h"
+
+int main (int argc, char **argv)
+{   
+  int dt=10; //seconds
+  int tempIndex=0;
+  if(argc>1) {
+    dt=atoi(argv[1]);
+  }
+
+  printf("unixTime\t");
+  for(tempIndex=0;tempIndex<NUM_SBS_TEMPS;tempIndex++) {
+    printf("%s\t",getSbsTemperatureLabel(tempIndex));
+  }  
+
+  while(1) {
+    printf("%u\t",time(&null));
+    for(tempIndex=0;tempIndex<NUM_SBS_TEMPS;tempIndex++) {
+      printf("%d\t",getSbsTemperature(tempIndex));
+    }  
+    printf("\n");
+    sleep(dt);
+    
+  }
+  
+
+  return 0;
+}
+
