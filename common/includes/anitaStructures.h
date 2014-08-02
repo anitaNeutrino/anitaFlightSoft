44729402391a2e58e01593c0947081868285e694
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2014-08-02 20:37:08 +0000

    Adding L1 rates to TURF

--- common/includes/anitaStructures.h
+++ common/includes/anitaStructures.h
@@ -126,7 +126,7 @@
 #define VER_HK_SS 30
 #define VER_CMD_ECHO 30
 #define VER_MONITOR 31
-#define VER_TURF_RATE 34
+#define VER_TURF_RATE 35
 #define VER_LAB_PED 30
 #define VER_FULL_PED 30
 #define VER_SLOW_1 30
@@ -752,7 +752,8 @@ typedef struct {
   unsigned int unixTime;
   unsigned short ppsNum; ///<It's only updated every second so no need for sub-second timing
   unsigned short deadTime; ///<How much were we dead??
-  unsigned char l3Rates[PHI_SECTORS][2]; ///<x16 to get Hz
+  unsigned short l1Rates[PHI_SECTORS][2]; //
+  unsigned char l3Rates[PHI_SECTORS][2]; /// to get Hz
   unsigned short l1TrigMask; ///< As read from TURF (16-bit upper phi, lower phi)
   unsigned short l1TrigMaskH; ///< As read from TURF (16-bit upper phi, lower phi)
   unsigned short phiTrigMask; ///< 16 bit phi-sector mask
