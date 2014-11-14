2367dddc94f4fe3996a3da2002a51683300a9fb4
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2014-11-14 03:39:14 +0000

    Change to copying one openport file at a time

--- flightSoft/trunk/script/openportCopyScript.sh
+++ flightSoft/trunk/script/openportCopyScript.sh
@@ -8,7 +8,7 @@ OPENPORT_DEST_DIR=/anitaStorage/antarctica14/telem/openport
 while [ 1 ]; do
     if test `ls /tmp/openport/* | wc -l` -gt 0 ; then
 	
-	for file in /tmp/openport/*; do
+	for file in /tmp/openport/*/*; do
 	    rsync -avP --remove-source-files --bwlimit=100  $file  ${OPENPORT_USER}@${OPENPORT_DEST_IP}:${OPENPORT_DEST_DIR}/ > /tmp/openportCopy.log  
 	done
     else 
