7671c7193aee3a201ad336bd2f28bf1a4aa0160a
rjn <rjn@80a68109-d7fa-0310-9a6f-c08eca58abaa>
2014-11-21 05:34:10 +0000

    Latest scripts from the flight machine

--- flightSoft/trunk/script/openportCopyScript.sh
+++ flightSoft/trunk/script/openportCopyScript.sh
@@ -2,20 +2,28 @@
 
 OPENPORT_USER=radio
 OPENPORT_DEST_IP=192.168.1.2
-OPENPORT_DEST_DIR=/anitaStorage/antarctica14/telem/openport/
+OPENPORT_DEST_DIR=/storage/antarctica14/telem/openport/
 
 echo "openportCopyPid: $$"
 echo $$ > /tmp/openportCopyPid
 
 while [ 1 ]; do
-    if test `ls /tmp/openport/* | wc -l` -gt 0 ; then
+    if test `ls /tmp/openport | wc -l` -gt 0 ; then
 	cd /tmp/openport
+	COUNT_RUNS=`ls /tmp/openport | wc -l`
+	echo "There are $COUNT_RUNS runs waiting"
+	COUNT=0
 	for rundir in [0-9]*; do
+	    let COUNT=COUNT+1
 	    echo $rundir
 	    if test `ls /tmp/openport/$rundir | wc -l` -gt 0 ; then
 		for file in $rundir/*; do
 		    rsync -avP --remove-source-files --bwlimit=100  $file  ${OPENPORT_USER}@${OPENPORT_DEST_IP}:${OPENPORT_DEST_DIR}/$rundir/ > /tmp/openportCopy.log  
 		done
+	    fi
+	    if [ $COUNT -lt $COUNT_RUNS ]; then
+		echo "Could remove dir"
+		rmdir $rundir
 	    fi
 	done
     else 
