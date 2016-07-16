459c3754f0e80dd0e4142e1318813de645070bc6
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2016-07-16 14:12:10 -0500

    add modified version of Patrick's libTuff into flight tree

---
+++ common/tuffLib/tuffLib.c
@@ -0,0 +1,315 @@
+#include "tuffLib.h"
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <sys/select.h>
+
+static char buf[1024];
+
+struct tuff_dev 
+{
+  int fd; 
+  struct termios oldtio; 
+}; 
+
+int libtuff_reset(tuff_dev_t * d,
+		  unsigned int irfcm) {
+  sprintf(buf, "{\"reset\":%d}\n\r", irfcm);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_onCommand(tuff_dev_t * d,
+		      unsigned int irfcm,
+		      unsigned int channel,
+		      unsigned int notchMask) {
+  sprintf(buf, "{\"on\":[%d,%d,%d]}\n\r",irfcm, channel, notchMask);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_offCommand(tuff_dev_t * d,
+		       unsigned int irfcm,
+		       unsigned int channel,
+		       unsigned int notchMask) {
+  sprintf(buf, "{\"off\":[%d,%d,%d]}\n\r",irfcm,channel,notchMask);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_setChannelNotches(tuff_dev_t * d,
+			      unsigned int irfcm,
+			      unsigned int channel,
+			      unsigned int notchMask) {
+  unsigned int notch = notchMask & 0x7;
+  unsigned int notNotch = (~notchMask) & 0x7;
+  
+  sprintf(buf,"{\"on\":[%d,%d,%d],\"off\":[%d,%d,%d]}\n\r", irfcm, channel,notch,
+	  irfcm,channel,notNotch);
+  printf("sending %s\n", buf);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_updateCaps(tuff_dev_t * d,
+		       unsigned int irfcm,
+		       unsigned int channel) {
+  unsigned int tuffStack;
+  unsigned int cmd;
+
+  if (channel > 23) return -1;
+  
+  if (channel < 12) tuffStack = 0;
+  else {
+    tuffStack = 1;
+    channel -= 12;
+  }
+  if (channel < 6) {
+    cmd = (1<<channel) << 8;
+  } else {
+    cmd = ((1<<(channel-6)) << 8) | 0x4000;
+  }
+  cmd |= 0x60;
+  sprintf(buf, "{\"raw\":[%d,%d,%d]}\n\r",
+	    irfcm,
+	    tuffStack,
+	    cmd);
+  return write(d->fd, buf, strlen(buf));  
+}
+
+int libtuff_setCap(tuff_dev_t * d,
+		   unsigned int irfcm,
+		   unsigned int channel,
+		   unsigned int notch,
+		   unsigned int capValue) {
+  // build up the raw command, there's no nice function for this
+  unsigned int tuffStack;
+  unsigned int cmd;
+
+  if (channel > 23 || notch > 2 || capValue > 31) return -1;
+  
+  if (channel < 12) tuffStack = 0;
+  else {
+    tuffStack = 1;
+    channel -= 12;
+  }
+  if (channel < 6) {
+    cmd = (1<<channel) << 8;
+  } else {
+    cmd = ((1<<(channel-6)) << 8) | 0x4000;
+  }
+  // notch cmd (lower 8 bits) is
+  // 0 n1 n0 v4 v3 v2 v1 v0
+  // where n[1:0] is the notch
+  // and v[4:0] is the value
+  cmd |= capValue;
+  cmd |= (notch << 5);
+  sprintf(buf, "{\"raw\":[%d,%d,%d]}\n\r",
+	    irfcm,
+	    tuffStack,
+	    cmd);
+  printf("going to send %s\n", buf);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_setQuietMode(tuff_dev_t * d,
+			  bool quiet) {
+  sprintf(buf,"{\"quiet\":%d}\n\r", quiet);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_pingiRFCM(tuff_dev_t * d,
+		      unsigned int timeout,
+		      unsigned int numPing,
+		      unsigned int *irfcmList) {
+  char *p;
+  unsigned int pingsSeen;
+  int res;
+  unsigned int nb;
+  struct timeval tv;
+  fd_set set;
+  int rv;
+  unsigned char c;
+  unsigned int check_irfcm;
+  unsigned int i; 
+
+  switch(numPing) {
+  case 0: return 0;
+  case 1: sprintf(buf, "{\"ping\":[%d]}\n\r", irfcmList[0]); break;
+  case 2: sprintf(buf, "{\"ping\":[%d,%d]}\n\r",
+		  irfcmList[0],
+		  irfcmList[1]); break;
+  case 3: sprintf(buf, "{\"ping\":[%d,%d,%d]}\n\r",
+		  irfcmList[0],
+		  irfcmList[1],
+		  irfcmList[2]); break;
+  case 4: sprintf(buf, "{\"ping\":[%d,%d,%d,%d]}\n\r",
+		  irfcmList[0],
+		  irfcmList[1],
+		  irfcmList[2],
+		  irfcmList[3]); break;
+  default: return -1;
+  }
+  res = write(d->fd, buf, strlen(buf));
+  if (res < 0) return res;
+  // sent off the ping, now wait for response
+  pingsSeen = 0;
+  nb = 0;
+  while (pingsSeen != numPing) {
+    FD_ZERO(&set);
+    FD_SET(d->fd, &set);
+    tv.tv_sec = timeout;
+    tv.tv_usec = 0;
+    rv = select(d->fd+1,&set,NULL,NULL,&tv);
+    if (rv == 0) return pingsSeen;
+    if (rv == -1) return -1;
+    read(d->fd, &c, 1);
+    if (c == '\n') {
+      buf[nb] = 0;
+      p = strstr(buf, "pong");
+      if (p != NULL) {
+	p += 2+strlen("pong"); // move past '":'
+	check_irfcm = *p - '0';
+	for (i=0;i<numPing;i++) {
+	  if (irfcmList[i] == check_irfcm) {
+	    pingsSeen++;
+	    irfcmList[i] = 0xFFFFFFFF;
+	  }
+	}
+      }
+      nb = 0;
+    } else {
+      buf[nb] = c;
+      nb++;
+    }
+  }
+  return pingsSeen;
+}
+
+int libtuff_setNotchRange(tuff_dev_t * d,
+			   unsigned int notch,
+			   unsigned int phiStart,
+			   unsigned int phiEnd) {
+  const char *notchStr[3] = { "r0", "r1", "r2" };
+  if (notch > 2) return -1;
+
+  sprintf(buf, "{\"%s\":[%d,%d]}\n\r", notchStr[notch], phiStart,phiEnd);
+  return write(d->fd, buf, strlen(buf));
+}
+
+int libtuff_setPhiSectors(tuff_dev_t * d,
+			  unsigned int irfcm,
+			  unsigned int *phiList,
+			  unsigned int nb) {
+  char *p;
+  unsigned int i;
+  int cnt;
+  p = buf;
+  cnt = sprintf(buf, "{\"set\":{\"irfcm\":%d,\"phi\":[", irfcm);
+  p += cnt;
+  for (i=0;i<nb;i++) {
+    if (i != 0) {
+      cnt = sprintf(p, ",");
+      p += cnt;
+    }
+    cnt = sprintf(p, "%d", phiList[i]);
+    p += cnt;
+  }
+  sprintf(p, "]}}\n\r");
+  return write(d->fd, buf, strlen(buf));
+}
+
+void libtuff_waitForAck(tuff_dev_t * d,
+			unsigned int irfcm) {
+  unsigned char nb;
+  unsigned char c;
+  char *p;
+  unsigned int check_irfcm;
+  nb = 0;
+  while (1) {
+    read(d->fd, &c, 1);
+    if (c == '\n') {
+      buf[nb] = 0;
+      p = strstr(buf, "ack");
+      if (p != NULL) {
+	p += 2+strlen("ack"); // move past '":'
+	check_irfcm = *p - '0';
+	if (check_irfcm == irfcm) break;
+      }
+      nb = 0;
+    } else {
+      buf[nb] = c;
+      nb++;
+    }
+  }  
+}
+
+
+
+#define BAUDRATE B115200
+
+
+tuff_dev_t * libtuff_open(const char * dev) 
+{
+  char sbuf[512]; 
+  struct termios tio;; 
+  int fd; 
+
+  fd = open(dev, O_RDWR | O_NOCTTY); 
+
+  if (fd < 0) 
+  {
+    snprintf(sbuf, sizeof(sbuf), "libtuff_open(%s)", dev); 
+    perror(sbuf); 
+    return NULL; 
+  }
+
+
+  /** initialize struct */ 
+  tuff_dev_t * ptr = malloc(sizeof(tuff_dev_t)); 
+  ptr->fd = fd; 
+
+  /* save old settings */ 
+  tcgetattr(fd, &ptr->oldtio); 
+
+  
+  /* set it up the way we like it */ 
+  tio.c_cflag = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD; 
+  tio.c_iflag = IGNPAR | ICRNL;
+  tio.c_oflag = 0;
+  tio.c_lflag = ICANON;
+  tio.c_cc[VINTR]    = 0;
+  tio.c_cc[VQUIT]    = 0;
+  tio.c_cc[VERASE]   = 0;
+  tio.c_cc[VKILL]    = 0;
+  tio.c_cc[VEOF]     = 4;
+  tio.c_cc[VTIME]    = 0; 
+  tio.c_cc[VMIN]     = 1;
+  tio.c_cc[VSWTC]    = 0;
+  tio.c_cc[VSTART]   = 0;
+  tio.c_cc[VSTOP]    = 0;
+  tio.c_cc[VSUSP]    = 0;
+  tio.c_cc[VEOL]     = 0;
+  tio.c_cc[VREPRINT] = 0;
+  tio.c_cc[VDISCARD] = 0;
+  tio.c_cc[VWERASE]  = 0;
+  tio.c_cc[VLNEXT]   = 0;
+  tio.c_cc[VEOL2]    = 0;
+
+  tcflush(fd, TCIFLUSH);
+  tcsetattr(fd,TCSANOW,&tio);
+  return ptr; 
+}
+
+
+int libtuff_close(tuff_dev_t * d) 
+{
+  if(!d) return 1; 
+  tcsetattr(d->fd,TCSANOW,&d->oldtio);
+  close(d->fd); 
+  free(d); 
+  return 0; 
+}
+
+
+ 
+  
---
+++ common/tuffLib/tuffLib.h
@@ -0,0 +1,171 @@
+#ifndef _LIBTUFF_H_
+#define _LIBTUFF_H_
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*
+ * TUFF library.
+ *
+ * NOTES: tuff_dev_t is an opaque type, it should be created with libtuff_open and closed with libtuff_close. 
+ *
+ */
+
+
+
+struct tuff_dev; 
+typedef struct tuff_dev tuff_dev_t; 
+
+
+/** \brief Issues a reset to a specific iRFCM.
+ *
+ * Note: resets don't actually affect the iRFCM (the TUFF master)
+ * itself. They reset the microcontrollers on the TUFFs.
+ */
+int libtuff_reset(tuff_dev_t * d,
+		  unsigned int irfcm);
+
+/** \brief Turns on notches on a specific iRFCM channel. 
+ *
+ * This function turns on the notches listed in notchMask.
+ * It does not turn off the other notches.
+ */
+int libtuff_onCommand(tuff_dev_t * d,
+		      unsigned int irfcm,
+		      unsigned int channel,
+		      unsigned int notchMask);
+
+/** \brief Turns off notches on a specific iRFCM channel.
+ *
+ * This function turns off the notches listed in notchMask.
+ * It does not turn on the other notches.
+ */
+int libtuff_offCommand(tuff_dev_t * d,
+		       unsigned int irfcm,
+		       unsigned int channel,
+		       unsigned int notchMask);
+
+/** \brief Specifically sets the notch values on a given channel.
+ *
+ * This command gets TWO ACKs, because it sends an on command
+ * as well as an off command.
+ *
+ * Bitmask is what notches you want on.
+ */
+int libtuff_setChannelNotches(tuff_dev_t * d,
+			      unsigned int irfcm,
+			      unsigned int channel,
+			      unsigned int notchMask);
+
+/** \brief Sets a capacitor value using raw commands.
+ *
+ * This function sets the tunable capacitor on a notch on a
+ * specific channel. I don't expect these to be changed much,
+ * if at all, so these just use raw commands rather than
+ * anything intelligible.
+ *
+ * Anyway, the function works, so the readability isn't that
+ * important.
+ */
+int libtuff_setCap(tuff_dev_t * d,
+		   unsigned int irfcm,
+		   unsigned int channel,
+		   unsigned int notch,
+		   unsigned int capValue);
+
+/** \brief Makes the TUFFs stop acking all commands.
+ *
+ * In non-interactive mode, once a pong has been
+ * received from all TUFFs, it probably makes sense to
+ * go ahead and turn off the ACKs for each command.
+ *
+ * Quiet mode does this. So call libtuff_setQuietMode(serial_fd, 1);
+ *
+ * From then on, TUFFs will not acknowledge any commands
+ * other than a ping.
+ */
+int libtuff_setQuietMode(tuff_dev_t * d,
+			 bool quiet);
+
+/** \brief Looks for an acknolwedgement from a given iRFCM.
+ *
+ * After a command, a TUFF acknowledges it with a brief 'ack'
+ * as well as its iRFCM number.
+ *
+ * Note that it acknowledges ALL commands in the JSON message, not just once
+ * for the JSON message!
+ *
+ * This SHOULD NOT BE CALLED if the TUFFs are in quiet mode.
+ * It'll hang forever.
+ */
+void libtuff_waitForAck(tuff_dev_t * d,
+			unsigned int irfcm);
+
+/** \brief Saves the current capacitor values to default.
+ *
+ * Saves all 3 tunable capacitor values into information flash
+ * in the TUFF microcontrollers so they are now the new defaults.
+ */
+int libtuff_updateCaps(tuff_dev_t * d,
+		       unsigned int irfcm,
+		       unsigned int channel);
+
+/** \brief Pings a list of iRFCMs.
+ *
+ * To find out if the iRFCMs are present, ping a list of them
+ * using this function. It returns the number of pongs received.
+ *
+ * Timeout is in seconds.
+ */
+int libtuff_pingiRFCM(tuff_dev_t * d,
+		      unsigned int timeout,
+		      unsigned int numPing,
+		      unsigned int *irfcmList);
+
+/** \brief Ranged notch command.
+ * 
+ * Turns on notches for phi sectors between phiStart/phiEnd (inclusive).
+ * Turns off notches outside of that range.
+ *
+ * The range works modulo 16, so (0,15) turns on all phi sectors, and
+ * (15,0) turns on only phi sectors 15 and 0.
+ */
+int libtuff_setNotchRange(tuff_dev_t * d,
+			    unsigned int notch,
+			    unsigned int phiStart,
+			    unsigned int phiEnd);
+
+/** \brief Set phi sectors for an iRFCM.
+ *
+ * Sets the 24 TUFF channels on an iRFCM to the assigned phi sectors.
+ *
+ * You can pass less than 24 channels, but it always starts with
+ * channel 0, and unpassed phi sectors are unchanged.
+ *
+ * Note that phi sector assignments/notch range commands haven't
+ * been extensively tested yet.
+ */
+int libtuff_setPhiSectors(tuff_dev_t * d,
+			  unsigned int irfcm,
+			  unsigned int *phiList,
+			  unsigned int nb);
+
+
+
+/* \brief  Open serial device using correct settings , returning our opaque handle
+ *
+ * Returns a tuff_dev_t * or NULL if could not open port 
+ */
+tuff_dev_t * libtuff_open(const char * device) ; 
+
+
+/** \brief Close a tuff_dev. 
+ *
+ * This will free the memory and restore the serial port to its old settings
+ *
+ */
+int libtuff_close(tuff_dev_t * d); 
+
+
+#endif
